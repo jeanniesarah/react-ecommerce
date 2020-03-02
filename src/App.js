c92ad01c77b91f95cfccf6b9d3a2536e733a9a39
jeanniesarah <jeannie.wesell@gmail.com>
2020-03-02 08:59:08 -0500

    Start of homepage

--- src/App.js
+++ src/App.js
@@ -1,32 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      meaningOfLife: 47
-    }
-    this.props = props
-  }
+import HomePage from "./homepage.component";
 
-  handleClick = () => {
-    this.setState(
-      (prevState, prevProps) => {
-        return { meaningOfLife: prevState.meaningOfLife + prevProps.increment };
-      },
-      () => console.log(this.state.meaningOfLife)
-    );
-  };
+class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>{this.state.meaningOfLife}</p>
-          <button onClick={this.handleClick}>Update State</button>
-        </header>
+      <div>
+        <HomePage></HomePage>
       </div>
     );
   }
