4033ccf4ed8fc2ee779aebd211746c993b821e68
Jeannie <hello@jeannierombough.com>
2020-03-01 19:22:05 -0500

    asynchronous setState

--- src/App.js
+++ src/App.js
@@ -3,29 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
-  constructor () {
-    super()
+  constructor(props) {
+    super(props);
     this.state = {
       meaningOfLife: 47
     }
+    this.props = props
   }
 
   handleClick = () => {
-    this.setState({meaningOfLife: this.state.meaningOfLife + 1})
-  }
+    this.setState(
+      (prevState, prevProps) => {
+        return { meaningOfLife: prevState.meaningOfLife + prevProps.increment };
+      },
+      () => console.log(this.state.meaningOfLife)
+    );
+  };
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            {this.state.meaningOfLife}
-          </p>
-          <button
-            onClick={this.handleClick}
-          >
-            Update State
-          </button>
+          <p>{this.state.meaningOfLife}</p>
+          <button onClick={this.handleClick}>Update State</button>
         </header>
       </div>
     );
