a29bb2f3cee9abebdca96dbac09dbbcc11acca2b
Jeannie <hello@jeannierombough.com>
2020-03-02 18:54:57 -0500

    update with shop page, collection preview component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <HomePage></HomePage>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
